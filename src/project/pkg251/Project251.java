6dffffedb32cbeea87775ad038beb3d53cdc7ee6
Raghad Saleh <iraghad340@gmail.com>
2024-02-09 18:51:09 +0300

    This is test for Group (Raghad)

--- src/project/pkg251/Project251.java
+++ src/project/pkg251/Project251.java
@@ -8,5 +8,6 @@ public class Project251 {
     public static void main(String[] args) {
         
        System.out.println("Im Shroug Alharbi");
+       System.out.println("I'm Raghad Alotaibi");
     }
 }
