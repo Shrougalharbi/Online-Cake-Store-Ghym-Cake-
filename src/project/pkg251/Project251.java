14feb8c9d873349a3ec7794b4923ae5c184ab118
amsjf <amsjf@192.168.1.14>
2024-02-09 23:02:14 +0300

    Hi this is group project

--- src/project/pkg251/Project251.java
+++ src/project/pkg251/Project251.java
@@ -9,7 +9,7 @@ public class Project251 {
         
        System.out.println("Im Shroug Alharbi");
        System.out.println("I'm Raghad Alotaibi");
-       System.out.println("I'm Shahd ALghamdi");
+       System.out.println("I'm Shahd Alghamdi");
 
     }
 }
