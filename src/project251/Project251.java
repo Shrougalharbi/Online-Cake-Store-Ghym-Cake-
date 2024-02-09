2837d20ddfb5678bcb8612182b11290d88aa9d9a
narjes8866 <narjes.jh12@gmail.com>
2024-02-09 18:48:51 +0300

    test

---
+++ src/project251/Project251.java
@@ -0,0 +1,13 @@
+
+package project251;
+
+
+public class Project251 {
+
+    
+    public static void main(String[] args) {
+        
+       System.out.println("Im Shroug Alharbi");
+       System.out.println("I'm Narjes Althubyani");
+    }
+}
