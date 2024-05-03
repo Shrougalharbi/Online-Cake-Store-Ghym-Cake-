1947f25a7ac201d1a7ff8a5942a1db3bb50a896b
amsjf <amsjf@DESKTOP-SUT0QKT>
2024-05-03 04:38:41 +0300

    user inquriy

--- src/project/pkg251/UserInquriy.java
+++ src/project/pkg251/UserInquriy.java
@@ -1,21 +1,24 @@
-
 package project.pkg251;
 
 import java.util.Scanner;
+
 public class UserInquriy {
-     private String userName;
+
+    private String userName;
     private String userEmail;
     private int inquiryNumber;
-    private String inquiryQuestion;
 
-    public UserInquriy(String userName, String userEmail, int inquiryNumber, String inquiryQuestion) {
+    //==================================================================
+    //constructor of user information  
+    public UserInquriy(String userName, String userEmail, int inquiryNumber) {
         this.userName = userName;
         this.userEmail = userEmail;
         this.inquiryNumber = inquiryNumber;
-        this.inquiryQuestion = inquiryQuestion;
-        
-    }
 
+    }
+    //==================================================================
+
+//GETTERS AND SETTRS
     public String getUserName() {
         return userName;
     }
@@ -26,10 +29,6 @@ public class UserInquriy {
 
     public int getInquiryNumber() {
         return inquiryNumber;
-    }
-
-    public String getInquiryQuestion() {
-        return inquiryQuestion;
     }
 
     public void setUserName(String userName) {
@@ -43,23 +42,67 @@ public class UserInquriy {
     public void setInquiryNumber(int inquiryNumber) {
         this.inquiryNumber = inquiryNumber;
     }
+    //==================================================================
 
-    public void setInquiryQuestion(String inquiryQuestion) {
-        this.inquiryQuestion = inquiryQuestion;
+    //METHOOD TO START THE SERVICE AND TRANSFER IT TO APPROPRITE SERVICE 
+    static void start() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("____________________________________\n");
+
+        System.out.print("Please enter your name:");
+        String userName = input.next();
+        System.out.print("\n Please enter your email:");
+        String userEmail = input.next();
+        int inquiryNumber = (int) (Math.random() * 10);
+
+        System.out.println("\n If you want a live chat please Enter 1 : ");
+        System.out.println("Or If you need Emile please Enter 2 : ");
+        int user = input.nextInt();
+
+        // Create UserInquiry object 
+        UserInquriy inquriy = new UserInquriy(userName, userEmail, inquiryNumber);
+
+        while (true) {
+            switch (user) {
+                case 1:
+                    inquriy.liveChat();
+                    break;
+
+                case 2:
+                    inquriy.SendEmile();
+                    break;
+
+                default:
+                    System.out.println("Invalid input. Please enter 1 for live chat or 2 for email support.");
+                    user = input.nextInt();
+                    continue;
+
+            }
+            break;
+        }
+        System.out.println("_____________________________\n");
+        System.out.println("THANK YOU TO VISIT US :) ");
+        System.out.println("_____________________________\n");
+
     }
+    //==================================================================
 
+    //METHOOD TO CHAT WITH OWNER
     void liveChat() {
         Scanner Chat = new Scanner(System.in);
 
         System.out.println("_________________________");
-        System.out.println("Welcome "+ userName +" . How i can help you ? \n");
+        System.out.println("Welcome " + userName + " . How i can help you ? \n");
         String User = Chat.next();
         System.out.println("\n OK . we will solve your problem immediately . please check your emile");
     }
+    //==================================================================
 
+    //METHOOD TO SEND EMILE
     void SendEmile() {
         System.out.println("_________________________");
-        System.out.println("Welcome "+ userName +" . ");
+        System.out.println("Welcome " + userName + " . ");
         System.out.println("\n Our emile is : cakeStore111@gmail.com ");
     }
 }
